1367e28a15c618f0f09fa91f37ef79af81fc6043
Rafael Abensur <rafael.abensur@convenia.com.br>
2017-12-14 11:02:25 -0200

    change version

--- dist/vue-data-tablee.js
+++ dist/vue-data-tablee.js
@@ -1,5 +1,5 @@
 /*!
- * vue-data-tablee v0.10.0
+ * vue-data-tablee v0.10.1
  * (c) 2017-present Vitor Cavalcanti <vitorluizc@outlook.com> (https://vitorluizc.github.io)
  * Released under the MIT License.
  */
