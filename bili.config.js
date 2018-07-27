88201faa13eaf3af76fd5b4f88d6fcf3201dc2a4
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-07-27 17:42:29 -0300

    Upgrade bundler dependencies & Unversion distributions sources

--- bili.config.js
+++ bili.config.js
@@ -1,8 +1,15 @@
+const { name } = require('./package.json');
+const vue = require('rollup-plugin-vue').default;
+const css = require('rollup-plugin-css-only');
+
 module.exports = {
+  js: 'buble',
+  input: 'src/DataTable.vue',
   banner: true,
   format: ['umd', 'umd-min', 'cjs', 'es'],
-  plugins: ['vue'],
-  vue: {
-    css: `dist/vue-data-tablee.css`
-  }
-}
+  filename: name + '[suffix].js',
+  plugins: [
+    css({ output: `dist/${name}.css` }),
+    vue({ css: false })
+  ]
+};
